b3071ac467395062389cadbcad8e0cbfe0cfcf27
Akashi_SN <btorntireinvynriy@gmail.com>
2017-07-15 12:35:28 +0900

    メイン関数の修正

--- chunithm/chunithm.py
+++ chunithm/chunithm.py
@@ -14,7 +14,8 @@ class Calculate:
         self.user_friend_code = func.fetch_user_friend_code(self.user_id)
         self.user_hash = hashlib.sha256(str(self.user_friend_code).encode('utf8')).hexdigest()
         self.user_data_base = db.User(self.user_hash)
-        self.rate = []
+        self.rate = {}
+        self.user = {}
 
     def calculate_best_rate(self):
         """
@@ -111,7 +112,7 @@ class Calculate:
                         music_recent_date_list.pop()
                         music_recent_list = sorted(music_recent_date_list, key=lambda x: x["music_rate"], reverse=True)
 
-        rate = []
+        rate = {}
         for i, music in enumerate(music_recent_list):
             if i < 10:
                 rate["recent_rate_sum"] += music["music_rate"]
@@ -128,8 +129,8 @@ class Calculate:
         play_log = func.fetch_play_log(self.user_id)
         final_play_date = play_log['userPlaylogList'][0]['userPlayDate'][0:-2]
 
-        now_date = datetime.now().strptime("%Y-%m-%d %H:%M:%S")
-        user = {
+        now_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        user_info = {
             'user_name': user_data["userName"],
             'user_character_file_name': user_data["characterFileName"],
             'user_character_level': user_data["characterLevel"],
@@ -149,227 +150,47 @@ class Calculate:
             'user_final_play_date': final_play_date,
             'execute_date': now_date
         }
+        self.rate["display"] = user_data["playerRating"] / 100.0
+        self.rate["highest"] = user_data["highestRating"] / 100.0
+        self.user["user_play_count"] = user_data["playCount"]
+        self.user["user_name"] = user_data["userName"]
+        self.user["execute_date"] = now_date
+        self.user_data_base.update_user_info(user_info)
 
-        self.user_data_base.update_user_info(user)
+    def calculate_rate(self):
+        """
+        レートを計算してデータベースに保存する
+        """
+        rate = {
+            'rate_display': self.rate["display"],
+            'rate_highest': self.rate["highest"],
+            'rate_best': math.floor((self.rate["best"]["best_rate_sum"] /30) * 100) / 100,
+            'rate_recent': math.floor((self.rate["recent"]["recent_rate_sum"] / 10) * 100) / 100,
+            'rate_max': math.floor(((self.rate["best"]["best_rate_sum"] + self.rate["best"]["max_best_rate"] * 10 ) / 40) * 100) /100,
+            'user_play_count': self.user["user_play_count"],
+            'execute_date': self.user["execute_date"]
+        }
 
-# レートを計算してデータベースに保存する
-def CalcRate(userId):
-    '''レートを計算してデータベースに保存する'''
+        self.rate["admin"] = rate
+        self.user_data_base.update_rate(rate)
 
-    Base = db.LoadBaseRate()
-    FriendCode = func.Get_FriendCode(userId)
-    if FriendCode is None:
-        return None
-
-    Hash = hashlib.sha256(str(FriendCode).encode('utf8')).hexdigest()
-
-    Rating = {}
-    DataBase = db.UserDataBase(Hash)
-
-    # Best枠について
-    MusicIdList = func.Get_MusicIdList(userId)  # MusicIdのリストの取得
-    if MusicIdList is None:
-        return None
-    Musics = []
-    i = 0
-    for Level in range(2,4):
-        MusicBestScore = func.Get_DiffList(userId,"1990"+str(Level))  # エキスパート(19902)とマスター(19903)の曲別最大スコアのリストの取得
-        if MusicBestScore is None:
-            return None
-        for MusicId in MusicIdList[Level-2]:
-            for Music in MusicBestScore['userMusicList']:
-                if Music['musicId'] == MusicId:
-                    MusicDetail = Base.Get_BaseRate(MusicId,Level)
-                    if MusicDetail is None or MusicDetail['BaseRate'] is None:
-                        continue
-                    else:
-                        Dic = {
-                            'MusicId':MusicId,
-                            'Level':Level,
-                            'MusicName':MusicDetail['MusicName'],
-                            'Image':MusicDetail['Image'],
-                            'BaseRate':MusicDetail['BaseRate'],
-                            'Rate':func.Score2Rate(Music['scoreMax'],MusicDetail['BaseRate']),
-                            'Score':Music['scoreMax']
-                        }
-                        Musics.append(Dic)
-    #ソート
-    Best = sorted(Musics,key=lambda x:x["Rate"],reverse=True)
-    Rate = {'BestRate':0,'MaxBestRate':0}
-    for Music in Best:
-        if i < 30:
-            Music['MaxScore'] = None
-            Rate['BestRate'] += Music['Rate']
-            if i == 0:
-                Rate['MaxBestRate'] =  Music['Rate']
-            elif i == 29:
-                Rate['MinBestRate'] =  Music['Rate']
-        else:
-            if Music['Score'] >= 1007500:
-                Music['MaxScore'] = None
-            else:
-                MaxScore = func.Rate2Score(Music['BaseRate'],Rate['MinBestRate'])
-                if MaxScore <= 1007500 and MaxScore > 0 and MaxScore - Music['Score'] > 0:
-                    Music['MaxScore'] = MaxScore
-                else:
-                    Music['MaxScore'] = None
-        i+=1
-
-    #データーベースに保存
-    DataBase.SetBest(Best)
-
-    #Recent
-    Playlog = func.Get_PlayLog(userId)
-    if Playlog is None:
-        return None
-    Recent = DataBase.LoadRecent()
-    LevelMap = {'master':3,"expert":2}
-    FinalPlayDate = Playlog['userPlaylogList'][0]['userPlayDate'][0:-2]
-
-    Musics = []
-    for Play in Playlog['userPlaylogList'][0:30]:
-        if Play['levelName'] == 'expert' or Play['levelName'] == 'master':
-            MusicId = Base.Get_MusicId(Play['musicFileName'])
-            if MusicId is None:
-                continue
-            MusicDetail = Base.Get_BaseRate(MusicId,LevelMap[Play['levelName']])
-            if MusicDetail is None or MusicDetail['BaseRate'] is None:
-                continue
-            else:
-                Dic = {
-                    'MusicId':MusicId,
-                    'Level':LevelMap[Play['levelName']],
-                    'MusicName':MusicDetail['MusicName'],
-                    'Image':MusicDetail['Image'],
-                    'BaseRate':MusicDetail['BaseRate'],
-                    'Rate':func.Score2Rate(Play['score'],MusicDetail['BaseRate']),
-                    'Score':Play['score'],
-                    'PlayDate':Play['userPlayDate'][0:-2]
-                }
-                Musics.append(Dic)
-    if Recent is None:
-        #レート順にソート
-        Recent = sorted(Musics,key=lambda x:x['Rate'],reverse=True)
-    else:
-        #レート順にソート
-        Recent = sorted(Recent,key=lambda x:x['Rate'],reverse=True)
-        if len(Recent) > 10:
-            UserData = DataBase.LoadUser()
-            #UserDataがゼロではなかったら
-            if len(UserData):
-                OldDate = datetime.strptime(UserData[-1]['FinalPlayDate'], '%Y-%m-%d %H:%M:%S')
-                for Play in Musics:
-                    NowDate = datetime.strptime(Play['PlayDate'], '%Y-%m-%d %H:%M:%S')
-                    #最後に実行されたときの曲と現在の曲の新旧
-                    if NowDate > OldDate:
-                        #Recent枠の最小と比較
-                        if Play['Rate'] > Recent[9]['Rate']:
-                            #Recent枠の最小と入れ替え
-                            Recent[-1]['MusicId'] = Play['MusicId']
-                            Recent[-1]['Level'] = Play['Level']
-                            Recent[-1]['MusicName'] = Play['MusicName']
-                            Recent[-1]['Image'] = Play['Image']
-                            Recent[-1]['BaseRate'] = Play['BaseRate']
-                            Recent[-1]['Score'] = Play['Score']
-                            Recent[-1]['Rate'] = Play['Rate']
-                            Recent[-1]['PlayDate'] = Play['PlayDate']
-                        elif Play['Score'] >= 1007500:
-                            pass
-                        elif Play['Score'] >= Recent[-1]['Score']:
-                            pass
-                        else:
-                            #プレイ日時順にソート
-                            Recent = sorted(Recent,key=lambda x:datetime.strptime(x['PlayDate'], '%Y-%m-%d %H:%M:%S'),reverse=True)
-                            #Recent候補枠の一番古い曲と入れ替え
-                            Recent[-1]['MusicId'] = Play['MusicId']
-                            Recent[-1]['Level'] = Play['Level']
-                            Recent[-1]['MusicName'] = Play['MusicName']
-                            Recent[-1]['Image'] = Play['Image']
-                            Recent[-1]['BaseRate'] = Play['BaseRate']
-                            Recent[-1]['Score'] = Play['Score']
-                            Recent[-1]['Rate'] = Play['Rate']
-                            Recent[-1]['PlayDate'] = Play['PlayDate']
-                            #レート順にソート
-                            Recent = sorted(Recent,key=lambda x:x['Rate'],reverse=True)
-                    else:
-                        pass
-            else:
-                pass
-        else:
-            pass
-
-    RecentRates = 0
-    i = 0
-    for Music in Recent:
-        if i < 10:
-            RecentRates += Music['Rate']
-            i += 1
-
-    #ユーザーデータ
-    UserInfo = func.Get_UserData(userId)
-    if UserInfo is None:
-        return None
-    else:
-        UserInfo = UserInfo['userInfo']
-
-    NowDate = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    User = {
-        'TotalPoint':UserInfo['totalPoint'],
-        'TrophyType':UserInfo['trophyType'],
-        'WebLimitDate':UserInfo['webLimitDate'][0:-2],
-        'CharacterFileName':UserInfo['characterFileName'],
-        'FriendCount':UserInfo['friendCount'],
-        'Point':UserInfo['point'],
-        'PlayCount':UserInfo['playCount'],
-        'CharacterLevel':UserInfo['characterLevel'],
-        'TrophyName':UserInfo['trophyName'],
-        'ReincarnationNum':UserInfo['reincarnationNum'],
-        'UserName':UserInfo['userName'],
-        'Level':UserInfo['level'],
-        'FriendCode':FriendCode,
-        'Hash':Hash,
-        'FinalPlayDate':FinalPlayDate,
-        'ExecuteDate': NowDate
-    }
-
-
-    #データベースに保存
-    DataBase.SetRecent(Recent)
-
-    #レート計算
-    DispRate = (UserInfo['playerRating'] / 100.0)
-    BestRate = math.floor((Rate['BestRate'] / 30) * 100) /100
-    Rating = {
-        'DispRate':DispRate,
-        'HighestRating':(UserInfo['highestRating'] / 100.0),
-        'MaxRate':(math.floor(((Rate['BestRate'] + Rate['MaxBestRate'] * 10) / 40) * 100) / 100),
-        'BestRate':BestRate,
-        #'RecentRate':(math.floor(((DispRate * 40 - BestRate * 30) / 10) * 100) / 100),
-        'RecentRate':(math.floor((RecentRates /10)*100)/100) ,
-        'Credits':UserInfo['playCount'],
-        'ExecuteDate': NowDate
-    }
-    #データベースに保存
-    DataBase.SetRate(Rating)
-
-    #データベースに保存
-    DataBase.SetUser(User)
-
-    Admin = db.AdminDataBase()
-    Data = {
-        'UserName':UserInfo['userName'],
-        'FriendCode':FriendCode,
-        'Hash':Hash,
-        'Credits':UserInfo['playCount'],
-        'DispRate':DispRate,
-        'HighestRating':Rating['HighestRating'],
-        'MaxRate':Rating['MaxRate'],
-        'BestRate':BestRate,
-        'RecentRate':Rating['RecentRate'],
-    }
-    Admin.SetData(Data)
-
-    return Hash
+    def update_admin(self):
+        """
+        管理用データベースにユーザー情報を保存する
+        """
+        admin_data = db.Admin()
+        data = {
+            'user_name': self.user["user_name"],
+            'user_friend_code': self.user_friend_code,
+            'user_hash': self.user_hash,
+            'user_play_count': self.user["user_play_count"],
+            'rate_display': self.rate["admin"]["rate_display"],
+            'rate_highest': self.rate["admin"]["rate_highest"],
+            'rate_best': self.rate["admin"]["rate_best"],
+            'rate_recent': self.rate["admin"]["rate_recent"],
+            'rate_max': self.rate["admin"]["rate_max"]
+        }
+        admin_data.update_user_admin(data)
 
 #Best枠の時の表示
 def DispBest(Hash):
